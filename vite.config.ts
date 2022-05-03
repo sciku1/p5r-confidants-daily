b455a6171c5d49af3605aac283ef7a23d8df3266
Sciku <timothy.lopez38@gmail.com>
2022-05-03 16:34:08 -0600

    changed it back

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    origin: process.env.VUE_APP_BASE_ORIGIN
+  },
   plugins: [vue()],
   resolve: {
     alias: {
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    publicPath: './p5r-confidant-search/'
-};
