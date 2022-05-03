ddc677c86ab597d868760f892e91898658755da9
Sciku <timothy.lopez38@gmail.com>
2022-05-03 15:56:51 -0600

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || '',
   plugins: [vue()],
   resolve: {
     alias: {
