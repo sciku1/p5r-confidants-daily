19921ec0d15950bff751385e3cf6127fc936df65
Sciku <timothy.lopez38@gmail.com>
2022-05-03 16:46:54 -0600

    another try

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    origin: process.env.VUE_APP_BASE_ORIGIN
-  },
   plugins: [vue()],
   resolve: {
     alias: {
