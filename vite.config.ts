d48f22bea27fe209bc527ca0f0a6d766ad955f92
Sciku <timothy.lopez38@gmail.com>
2022-05-03 16:50:55 -0600

    more trying...

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/p5r-confidant-search",
+  base: "/p5r-confidant-search/",
+  server: {
+    origin: "/p5r-confidant-search/"
+  },
   plugins: [vue()],
   resolve: {
     alias: {
